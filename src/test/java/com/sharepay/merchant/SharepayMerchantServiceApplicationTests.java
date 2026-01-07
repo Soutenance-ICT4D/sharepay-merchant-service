c9b35912ac5e230bce073814aab0016345b9dd9b
DjineSP <dsintopafing@gmail.com>
2026-01-07 08:35:31 +0100

    Initialisation du service Marchant

---
+++ src/test/java/com/sharepay/merchant/SharepayMerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharepay.merchant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharepayMerchantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
